a8a4e465bfcd52fdef49717fe84313329fa6ae3d
herbertg14 <herbert.gutierrez@utexas.edu>
2016-08-27 16:36:06 -0500

    not adding duplicates

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,23 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-// router.get('/', function(req,res) {
-// 	res.render('index');
-// });
+
 var logger = require('morgan');
 var mongoose = require('mongoose');
-// Notice: Our scraping tools are prepared, too
+
 var request = require('request'); 
 var cheerio = require('cheerio');
+
 mongoose.connect('mongodb://localhost/week18test');
 var db = mongoose.connection;
 
-// show any mongoose errors
+
 db.on('error', function(err) {
   console.log('Mongoose Error: ', err);
 });
 
-// once logged in to the db through mongoose, log a success message
+
 db.once('open', function() {
   console.log('Mongoose connection successful.');
 });
@@ -25,51 +24,67 @@ db.once('open', function() {
 var Note = require('./../models/Note.js');
 var Article = require('./../models/Article.js');
 
-
-
 router.get('/', function(req, res) {
   res.render('index');
 });
 
-// A GET request to scrape the echojs website.
+
 router.get('/scrape', function(req, res) {
-	// first, we grab the body of the html with request
-  request('http://www.echojs.com/', function(error, response, html) {
-  	// then, we load that into cheerio and save it to $ for a shorthand selector
-    var $ = cheerio.load(html);
-    // now, we grab every h2 within an article tag, and do the following:
-    $('article h2').each(function(i, element) {
 
-    		// save an empty result object
-				var result = {};
+	Article.find({}, function(err, doc){
 
-				// add the text and href of every link, 
-				// and save them as properties of the result obj
-				result.title = $(this).children('a').text();
-				result.link = $(this).children('a').attr('href');
+		if (err){
+			console.log(err);
+		} 
 
-				// using our Article model, create a new entry.
-				// Notice the (result):
-				// This effectively passes the result object to the entry (and the title and link)
-				var entry = new Article (result);
+		//scrape site and don't add any duplicates
+		else {
+			var list = doc;
 
-				// now, save that entry to the db
-				entry.save(function(err, doc) {
-					// log any errors
-				  if (err) {
-				    console.log(err);
-				  } 
-				  // or log the doc
-				  else {
-				    console.log(doc);
-				  }
+			request('http://www.echojs.com/', function(error, response, html) {
+			// then, we load that into cheerio and save it to $ for a shorthand selector
+				var $ = cheerio.load(html);
+				// now, we grab every h2 within an article tag, and do the following:
+				$('article h2').each(function(i, element) {
+
+				// save an empty result object
+					var result = {};
+
+					// add the text and href of every link, 
+					// and save them as properties of the result obj
+					result.title = $(this).children('a').text();
+					result.link = $(this).children('a').attr('href');
+
+					var check = false;
+					for (var i = 0; i < doc.length; i++){
+						if (doc[i].title == result.title){
+							check = true;
+						}
+					}
+
+					if (check){
+					console.log("already in table");
+					}else{
+						var entry = new Article (result);
+
+						// now, save that entry to the db
+						entry.save(function(err, doc) {
+							// log any errors
+						  if (err) {
+						    console.log(err);
+						  } 
+						  // or log the doc
+						  else {
+						     console.log(doc);
+						  }
+						});
+					}
 				});
+			});
+		}
+	});
 
-
-    });
-  });
-  // tell the browser that we finished scraping the text.
-  res.send("Scrape Complete");
+	res.send("Scrape Complete");
 });
 
 // this will get the articles we scraped from the mongoDB

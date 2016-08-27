304d7d0b4cd3ec1ac76ffe9617f57afd77566828
herbertg14 <herbert.gutierrez@utexas.edu>
2016-08-27 18:14:02 -0500

    cleaned up code

--- controllers/controller.js
+++ controllers/controller.js
@@ -38,21 +38,15 @@ router.get('/scrape', function(req, res) {
 			console.log(err);
 		} 
 
-		//scrape site and don't add any duplicates
 		else {
 			var list = doc;
 
 			request('http://www.echojs.com/', function(error, response, html) {
-			// then, we load that into cheerio and save it to $ for a shorthand selector
 				var $ = cheerio.load(html);
-				// now, we grab every h2 within an article tag, and do the following:
 				$('article h2').each(function(i, element) {
 
-				// save an empty result object
 					var result = {};
 
-					// add the text and href of every link, 
-					// and save them as properties of the result obj
 					result.title = $(this).children('a').text();
 					result.link = $(this).children('a').attr('href');
 
@@ -68,13 +62,10 @@ router.get('/scrape', function(req, res) {
 					}else{
 						var entry = new Article (result);
 
-						// now, save that entry to the db
 						entry.save(function(err, doc) {
-							// log any errors
 						  if (err) {
 						    console.log(err);
 						  } 
-						  // or log the doc
 						  else {
 						     console.log(doc);
 						  }
@@ -90,13 +81,11 @@ router.get('/scrape', function(req, res) {
 
 // this will get the articles we scraped from the mongoDB
 router.get('/articles', function(req, res){
-	// grab every doc in the Articles array
 	Article.find({}, function(err, doc){
-		// log any errors
+
 		if (err){
 			console.log(err);
 		} 
-		// or send the doc to the browser as a json object
 		else {
 			// var hbsObject = {article: doc[0]};
 			// console.log(hbsObject);
@@ -106,20 +95,14 @@ router.get('/articles', function(req, res){
 	});
 });
 
-// grab an article by it's ObjectId
 router.get('/articles/:id', function(req, res){
-	// using the id passed in the id parameter, 
-	// prepare a query that finds the matching one in our db...
+
 	Article.findOne({'_id': req.params.id})
-	// and populate all of the notes associated with it.
 	.populate('note')
-	// now, execute our query
 	.exec(function(err, doc){
-		// log any errors
 		if (err){
 			console.log(err);
 		} 
-		// otherwise, send the doc to the browser as a json object
 		else {
 			res.json(doc);
 		}
@@ -127,31 +110,19 @@ router.get('/articles/:id', function(req, res){
 });
 
 
-// replace the existing note of an article with a new one
-// or if no note exists for an article, make the posted note it's note.
 router.post('/articles/:id', function(req, res){
-	// create a new note and pass the req.body to the entry.
 	var newNote = new Note(req.body);
 
-	// and save the new note the db
 	newNote.save(function(err, doc){
-		// log any errors
 		if(err){
 			console.log(err);
 		} 
-		// otherwise
 		else {
-			// using the Article id passed in the id parameter of our url, 
-			// prepare a query that finds the matching Article in our db
-			// and update it to make it's lone note the one we just saved
 			Article.findOneAndUpdate({'_id': req.params.id}, {'note':doc._id})
-			// execute the above query
 			.exec(function(err, doc){
-				// log any errors
 				if (err){
 					console.log(err);
 				} else {
-					// or send the document to the browser
 					res.send(doc);
 				}
 			});
--- server.js
+++ server.js
@@ -4,43 +4,22 @@ var methodOverride = require('method-override');
 
 var logger = require('morgan');
 var mongoose = require('mongoose');
-// Notice: Our scraping tools are prepared, too
+
 var request = require('request'); 
 var cheerio = require('cheerio');
 
 
 var app = express();
 
-//Serve static content for the app from the "public" directory in the application directory.
+
 app.use(express.static(process.cwd() + '/public'));
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({
 	extended: false
 }));
-// override with POST having ?_method=DELETE
+
 app.use(methodOverride('_method'));
 var exphbs = require('express-handlebars');
-
-
-/////////////////////////////////////////
-// mongoose.connect('mongodb://localhost/week18hw');
-// var db = mongoose.connection;
-
-// // show any mongoose errors
-// db.on('error', function(err) {
-//   console.log('Mongoose Error: ', err);
-// });
-
-// // once logged in to the db through mongoose, log a success message
-// db.once('open', function() {
-//   console.log('Mongoose connection successful.');
-// });
-
-
-// // And we bring in our Note and Article models
-// var Note = require('./models/Note.js');
-// var Article = require('./models/Article.js');
-///////////////////////////////////////
 
 
 app.engine('handlebars', exphbs({
